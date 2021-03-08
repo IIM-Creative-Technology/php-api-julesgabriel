a2822224f26e28b670ea8bda2e2567f628fb6296
Jules DAYAUX <julesdayaux@mbp-de-jules.home>
2021-03-08 21:18:04 +0100

    ADD entities and jwt generated on auth login

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\PromotionRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api")
+ */
+class ApiController extends AbstractController
+{
+    private $objectManager;
+    public function __construct(EntityManagerInterface $objectManager, RequestStack $request){
+        $apiToken = $request->getCurrentRequest()->headers->get('api_token');
+        $user = $this->objectManager->getRepository(User::class)->findOneBy([
+            'apiKey' => $apiToken,
+        ]);
+    }
+
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+
+    /**
+     * @Route("/promotion", name="promotion_findAll", methods={"GET"})
+     * @param PromotionRepository $promotionRepository
+     * @return Response
+     */
+
+    public function findAllPromotion(PromotionRepository $promotionRepository): Response
+    {
+        $promotions = $promotionRepository->findAll();
+        $arrayCollection = array();
+        foreach($promotions as $promotion) {
+            $arrayCollection = array(
+                'id'                => $promotion->getId(),
+                'start'   => $promotion->getStart(),
+                'exit' => $promotion->getExit()
+            );
+        }
+        $response = new JsonResponse($arrayCollection);
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+
+    }
+}
